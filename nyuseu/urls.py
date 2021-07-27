4949e79d96b685583a61411be03040c32afd429c
FoxMaSk <foxmask@protonmail.com>
2021-07-27 22:48:14 +0200

    error pages

--- nyuseu/urls.py
+++ nyuseu/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import handler404, handler500
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin

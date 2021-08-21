20e9ef64ddebf5fb20727273efd33103e703c86f
FoxMaSk <foxmask@protonmail.com>
2021-08-21 08:55:48 +0200

    API Doc w/ swagger

--- nyuseu/api/urls.py
+++ nyuseu/api/urls.py
@@ -3,8 +3,10 @@
 Nyuseu - News - 뉴스
 """
 from django.urls import include, path
-from rest_framework import routers
+from django.views.generic import TemplateView
 from nyuseu.api.views import ArticlesViewSet, FeedsViewSet, FoldersViewSet
+from rest_framework import routers
+from rest_framework.schemas import get_schema_view
 
 router = routers.DefaultRouter()
 router.register(r'articles', ArticlesViewSet)
@@ -15,4 +17,15 @@ urlpatterns = [
     path('v1/', include(router.urls)),
     # path('v2/', include(router.urls)),
 
+    # API DOC - begin
+    path('v1/swagger-ui/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url': 'openapi-schema'}
+    ), name='swagger-ui'),
+    path('v1/openapi', get_schema_view(
+        title="Nyuseu - News - 뉴스",
+        description="Nyuseu API",
+        version="0.2.0"
+    ), name='openapi-schema'),
+    # API DOC - end
 ]
--- nyuseu/api/views.py
+++ nyuseu/api/views.py
@@ -3,35 +3,50 @@
 Nyuseu - News - 뉴스
 """
 from nyuseu.api.serializers import ArticlesSerializer, FeedsSerializer, FoldersSerializer
-
 from nyuseu.models import Articles, Feeds, Folders
 
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
 
 class ArticlesViewSet(viewsets.ModelViewSet):
     """
-        ViewSet for listing or retrieving articles
-        available methods are
-        /GET .list(), .retrieve(),
-        /POST .create(),
-        /UPDATE .update(),
-        /PATCH .partial_update(), and
-        /DELETE .destroy()
+        ViewSet for creating, updating, listing, retrieving, deleting articles
     """
     serializer_class = ArticlesSerializer
     queryset = Articles.objects.all()
 
+    def create(self, request, *args, **kwargs):
+        feeds_str = request.data['feeds']
+        feeds = Feeds.objects.get(title=feeds_str)
+        request.data['feeds'] = feeds.id
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def update(self, request, *args, **kwargs):
+        feeds_str = request.data['feeds']
+        feeds = Feeds.objects.get(title=feeds_str)
+        request.data['feeds'] = feeds.id
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
+
 
 class FeedsViewSet(viewsets.ModelViewSet):
     """
-        ViewSet for listing or retrieving Feeds
-        available methods are
-        /GET .list(), .retrieve(),
-        /POST .create(),
-        /UPDATE .update(),
-        /PATCH .partial_update(), and
-        /DELETE .destroy()
+        ViewSet for creating, updating, listing, retrieving, deleting Feeds
     """
     serializer_class = FeedsSerializer
     queryset = Feeds.objects.all()
@@ -39,13 +54,7 @@ class FeedsViewSet(viewsets.ModelViewSet):
 
 class FoldersViewSet(viewsets.ModelViewSet):
     """
-        ViewSet for listing or retrieving Folders
-        available methods are
-        /GET .list(), .retrieve(),
-        /POST .create(),
-        /UPDATE .update(),
-        /PATCH .partial_update(), and
-        /DELETE .destroy()
+        ViewSet for creating, updating, listing, retrieving, deleting Folders
     """
     serializer_class = FoldersSerializer
     queryset = Folders.objects.all()

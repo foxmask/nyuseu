0de905049e732f450b2923805a2023d09d0c89ee
FoxMaSk <foxmask@protonmail.com>
2021-12-02 21:57:15 +0100

    when no data retrieved

--- nyuseu/engine.py
+++ nyuseu/engine.py
@@ -146,26 +146,58 @@ def get_image(entry, content):
     return str(new_image)
 
 
+def update_feeds(feed_id, now):
+    """
+        Update the read feeds
+    """
+    source_feeds = Feeds.objects.get(id=feed_id)
+    source_feeds.date_grabbed = now
+    source_feeds.save()
+
+
+def add_article(my_feed, entry, created_entries, now):
+    """
+        Add a new article
+    """
+    content, image = set_content(entry)
+    if image == '':
+        image = "<img class=\"card-img-top\" />"
+    # add an article
+    res = Articles(title=entry.title,
+                   text=str(content),
+                   image=str(image),
+                   feeds=my_feed,
+                   source_url=entry.link)
+    try:
+        res.full_clean()  # to call the UniqueConstraint
+        res.save()
+        created_entries += 1
+        update_feeds(my_feed.id, now)
+        console.print(f'Feeds {my_feed.title} : {entry.title}', style="blue")
+        return created_entries
+    except ValidationError:
+        pass
+
+
 def go():
     """
         get the data of each RSS Feeds, then create `Articles`
     """
     console.print('Nyuseu Engine - 뉴스 - Feeds Reader - in progress', style="green")
     feeds = Feeds.objects.all()
-    for my_feeds in feeds:
+    for my_feed in feeds:
         rss = Rss()
-        console.print(f"Feeds {my_feeds.url}", style="magenta")
-        feeds = rss.get_data(**{'url_to_parse': my_feeds.url, 'bypass_bozo': settings.BYPASS_BOZO})
-        if hasattr(feeds, 'entries') is False:
+        console.print(f"Feeds {my_feed.url}", style="magenta")
+        feeds_data = rss.get_data(**{'url_to_parse': my_feed.url, 'bypass_bozo': settings.BYPASS_BOZO})
+        if hasattr(feeds_data, 'entries') is False:
             continue
         now = arrow.utcnow().to(settings.TIME_ZONE).format('YYYY-MM-DDTHH:mm:ssZZ')
-        date_grabbed = arrow.get(my_feeds.date_grabbed).format('YYYY-MM-DDTHH:mm:ssZZ')
+        date_grabbed = arrow.get(my_feed.date_grabbed).format('YYYY-MM-DDTHH:mm:ssZZ')
         read_entries = 0
         created_entries = 0
-        for entry in feeds.entries:
+        for entry in feeds_data.entries:
             # it may happened that feeds does not provide title ... yes !
-            if 'title' not in entry:
-                entry['title'] = entry.link
+            entry['title'] = entry.link if 'title' not in entry else entry['title']
             read_entries += 1
             # entry.*_parsed may be None when the date in a RSS Feed is invalid
             # so will have the "now" date as default
@@ -174,30 +206,11 @@ def go():
                 published = arrow.get(published).to(settings.TIME_ZONE).format('YYYY-MM-DDTHH:mm:ssZZ')
             # last triggered execution
             if published is not None and now >= published >= date_grabbed:
-                content, image = set_content(entry)
-                if image == '':
-                    image = "<img class=\"card-img-top\" />"
-                # add an article
-                res = Articles(title=entry.title,
-                               text=str(content),
-                               image=str(image),
-                               feeds=my_feeds,
-                               source_url=entry.link)
-                try:
-                    res.full_clean()  # to call the UniqueConstraint
-                    res.save()
-                    created_entries += 1
-                    now = arrow.utcnow().to(settings.TIME_ZONE).format('YYYY-MM-DD HH:mm:ssZZ')
-                    source_feeds = Feeds.objects.get(id=my_feeds.id)
-                    source_feeds.date_grabbed = now
-                    source_feeds.save()
-                    console.print(f'Feeds {my_feeds.title} : {entry.title}', style="blue")
-                except ValidationError:
-                    pass
+                add_article(my_feed, entry, created_entries)
 
         if read_entries:
-            console.print(f'{my_feeds.title}: Entries created {created_entries} / Read {read_entries}', style="magenta")
+            console.print(f'{my_feed.title}: Entries created {created_entries} / Read {read_entries}', style="magenta")
         else:
-            console.print(f'{my_feeds.title}: no feeds read', style="blue")
+            console.print(f'{my_feed.title}: no feeds read', style="blue")
 
     console.print('Nyuseu Engine - 뉴스 - Feeds Reader - Finished!', style="green")
